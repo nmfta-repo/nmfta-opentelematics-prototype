e2275f3b78279a2e272424007833e8d976ca4196
Joanne Popper <jpopper@inceptconsulting.com>
2019-04-09 12:16:57 -0500

    Revert "Remove APIModelBase from ProviderStatusModel and IncidentModel"
    This reverts commit 21055596bbe94f0ee21bc91363695795bb1cd305.

--- Prototype.OpenTelematics.Models/ProviderStatusModel.cs
+++ Prototype.OpenTelematics.Models/ProviderStatusModel.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace Prototype.OpenTelematics.Models
 {
-    public class ProviderStatusModel 
+    public class ProviderStatusModel : ApiModelBase
     {
         public string serviceStatus { get; set; }
         public DateTimeOffset dateTime { get; set; }
         public string[] factors { get; set; }
     }
 
-    public class IncidentsModel 
+    public class IncidentsModel : ApiModelBase
     {
         public List<ProviderStatusModel> data { get; set; }
     }
