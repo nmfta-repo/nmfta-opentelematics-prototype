21055596bbe94f0ee21bc91363695795bb1cd305
Joanne Popper <jpopper@inceptconsulting.com>
2019-04-09 12:16:21 -0500

    Remove APIModelBase from ProviderStatusModel and IncidentModel

---
+++ Prototype.Telematics.BridgeServer/dbo/Tables/VehiclePerformanceThreshold.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[VehiclePerformanceThreshold]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [providerId] UNIQUEIDENTIFIER NULL, 
+    [activeFrom] DATETIMEOFFSET NULL, 
+    [activeTo] DATETIMEOFFSET NULL, 
+    [rpmOverValue] INT NULL, 
+    [overSpeedValue] INT NULL, 
+    [excessSpeedValue] INT NULL, 
+    [longIdleValue] INT NULL, 
+    [hiThrottleValue] INT NULL
+)
